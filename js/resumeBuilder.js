59c7b20070ca4fdce4be10284e4f5d9a61969eed
Greg Fells <gfells@live.com>
2018-08-19 14:27:52 -0600

    Added work and projects objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,4 @@
-var bio, education, work, projects;
-
-bio = {
+var bio = {
   name: "Greg Fells",
   role: "Front End Developer",
   contacts: {
@@ -13,10 +11,10 @@ bio = {
   welcomeMessage: "I'm working on it bruh",
   skills: ["This", "Will need", "To be", "Updated when I have some"],
   biopic: "images/me.png",
-  display: "function",
+  display: function(){};
 };
 
-education = {
+var education = {
   schools: [
     {
       name: "Arapahoe Community College",
@@ -38,14 +36,55 @@ education = {
       title: "Intro to Programming Nanodegree",
       school: "Udacity",
       dates: "2017",
-      url: "http://udacity.com"
+      url: "udacity.com"
     },
     {
       title: "Front-End Web Developmer Nanodegree",
       school: "Udacity",
       dates: "2018",
-      url: "http://udacity.com"
+      url: "udacity.com"
     }
-  ]
-  display: function
+  ],
+  display: function(){};
+};
+
+var work = {
+  jobs: [
+    {
+      employer: "CDR Construction",
+      title: "Partner",
+      location: "Denver, CO",
+      dates: "2007 - Present",
+      description: "The 'Son' in a 'Father and Son' construction business. Helped \
+      manage billing, clients, materials, and jobs, as well as performing labor \
+      and acting as foreman of small crews."
+    },
+    {
+      employer: "Safeway Inc.",
+      title: "All Purpose Clerk",
+      location: "Englewood, CO",
+      dates: "July 2007 - Jan 2011",
+      description: "Started as a courtesy clerk, where I assisted customers, did \
+      basic cleaning tasks, and performed minor inventory maintenance. After 4 years \
+      I had been the GMHBC manager, front end manager, and ended as the fuel station \
+      manager. Also worked in most other departments, including bookkeeping. I was \
+      the acting Fuel Station manager, though I often stepped in as manager of other \
+      departments to fill in for sick leave, vacations, or other vacancies."
+    }
+  ],
+  display: function(){};
+};
+
+var projects = {
+  projects:[
+    {
+      title: "I'm glad I can put fake info here",
+      dates: "2017 - 2018"
+      description: "I don't really want to use this as a resume, probably, so I \
+      don't want to take the time to actually set my projects in here. This is just \
+      placeholder information!"
+      images: ["placeholderurl", "andanotherone.com"]
+    }
+  ],
+  display: function(){};
 };
