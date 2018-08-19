475b435a37ad914a1253e97c6575915894f784e4
Greg Fells <gfells@live.com>
2018-08-19 13:53:18 -0600

    Added bio and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,51 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio, education, work, projects;
+
+bio = {
+  name: "Greg Fells",
+  role: "Front End Developer",
+  contacts: {
+       mobile: "REDACTED",
+       email: "gfells@live.com",
+       github: "getPetrafied",
+       twitter: "REDACTED",
+       location: "Denver, CO"
+     },
+  welcomeMessage: "I'm working on it bruh",
+  skills: ["This", "Will need", "To be", "Updated when I have some"],
+  biopic: "images/me.png",
+  display: "function",
+};
+
+education = {
+  schools: [
+    {
+      name: "Arapahoe Community College",
+      location: "Arapahoe, CO",
+      degree: "Associate of Arts: Psychology",
+      majors: "Psychology",
+      dates: "2010 - 2013",
+    },
+    {
+      name: "University of Colorado Denver",
+      location: "Denver, CO",
+      degree: "Bachelor of Arts: Nueropsychology",
+      majors: "Nueropsychology",
+      dates: "2013 - 2015",
+    }
+  ],
+  onlineCourses: [
+    {
+      title: "Intro to Programming Nanodegree",
+      school: "Udacity",
+      dates: "2017",
+      url: "http://udacity.com"
+    },
+    {
+      title: "Front-End Web Developmer Nanodegree",
+      school: "Udacity",
+      dates: "2018",
+      url: "http://udacity.com"
+    }
+  ]
+  display: function
+};
